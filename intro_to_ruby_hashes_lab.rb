d5fa2188f0beaa383069807d9f5f431efcfe4210
Dean L Betty <deanlloyd64@gmail.com>
2019-09-02 20:55:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   {:railroads => 
     {:pieces => 4, 
       :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}, 
-      :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :key_7 => 0, :key_8 => 0}
+      :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => 0, :key_8 => 0}
     }
     
   }
