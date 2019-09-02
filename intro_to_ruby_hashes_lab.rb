d3154ed9301e8ed37a7e1aac57b62ce28ccac68c
Dean L Betty <deanlloyd64@gmail.com>
2019-09-02 21:12:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,9 +40,9 @@ def monopoly_with_fourth_tier
                           :three_pieces_owned => 100,
                           :four_pieces_owned => 200}, 
       :names => {:reading_railroad => {mortgage_value: "$100"}, 
-                :pennsylvania_railroad => {mortgage_value => "$"}, 
-                :b_and_o_railroad => {mortgage_value => "$"},
-                :shortline_railroad => {mortgage_value => "$"}
+                :pennsylvania_railroad => {mortgage_value: "$"}, 
+                :b_and_o_railroad => {mortgage_value: "$"},
+                :shortline_railroad => {mortgage_value: "$"}
         
       }
     }
