478e936d5271e0ff3bf2d915e563f95e0708efe3
Dean L Betty <deanlloyd64@gmail.com>
2019-09-02 20:53:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
   # complexity.
   {:railroads => 
     {:pieces => 4, 
-      :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 0, :key_4 => 0}, 
+      :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :key_4 => 0}, 
       :names => {:key_5 => 0, :key_6 => 0, :key_7 => 0, :key_8 => 0}
     }
     
